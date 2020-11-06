a18e04ca2d975dbfc72c58fff1b3932861f496e4
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2020-11-06 18:23:59 -0500

    Adds compression and encryption to chunks

--- src/server/pkg/storage/chunk/client.go
+++ src/server/pkg/storage/chunk/client.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	fmt "fmt"
 	io "io"
-	"io/ioutil"
 	"path"
 	"strings"
 	"sync"
@@ -55,11 +54,7 @@ func NewClient(objc obj.Client, mdstore MetadataStore, tracker tracker.Tracker, 
 	return c
 }
 
-func (c *Client) Create(ctx context.Context, md Metadata, r io.Reader) (ID, error) {
-	chunkData, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
+func (c *Client) Create(ctx context.Context, md Metadata, chunkData []byte) (ID, error) {	
 	chunkID := Hash(chunkData)
 	var pointsTo []string
 	for _, cid := range md.PointsTo {
